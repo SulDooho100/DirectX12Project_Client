6d9c7da8aab5895de3cdffe8a2a6ba9e547efaa1
설두호\SulDooho <suldooho100@gmail.com>
2025-05-08 21:09:43 +0900

    Created DeviceManager class

---
+++ DirectX12Project_Client/DirectX12Project_Client/Core/DeviceManager.cpp
@@ -0,0 +1,69 @@
+#include "stdafx.h"
+#include "DeviceManager.h"
+
+DeviceManager& DeviceManager::GetInstance()
+{
+	static DeviceManager instance;
+	return instance;
+}
+
+void DeviceManager::Initialize()
+{
+    try
+    {
+        CreateFactory();
+        CreateAdapter();
+        CreateDevice();
+
+    }
+    catch (std::wstring message)
+    {
+        std::wcout << message << std::endl;
+    }
+}
+
+ID3D12Device* DeviceManager::GetDevice()
+{
+	return device_.Get();
+}
+
+void DeviceManager::CreateFactory()
+{
+#if defined(DEBUG) || defined(_DEBUG)
+    {
+        Microsoft::WRL::ComPtr<ID3D12Debug> debug_controller;
+        D3D12GetDebugInterface(IID_PPV_ARGS(&debug_controller));
+        debug_controller->EnableDebugLayer();
+    }
+#endif
+
+    THROW_IF_FAILED(CreateDXGIFactory2(0, IID_PPV_ARGS(&factory_)));
+}
+
+void DeviceManager::CreateAdapter()
+{
+    Microsoft::WRL::ComPtr<IDXGIAdapter4> adapter;
+
+    for (unsigned int i = 0; factory_->EnumAdapterByGpuPreference(i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&adapter)) != DXGI_ERROR_NOT_FOUND; ++i) 
+    { 
+        DXGI_ADAPTER_DESC1 adapter_desc;
+        adapter->GetDesc1(&adapter_desc);
+
+        if (!(adapter_desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)) 
+        {
+            adapter_ = adapter; 
+
+            break;
+        }
+    }
+
+    if (adapter_ == nullptr)
+    {
+        THROW_IF_FAILED(DXGI_ERROR_NOT_FOUND);
+    }
+}
+
+void DeviceManager::CreateDevice()
+{ 
+    THROW_IF_FAILED(D3D12CreateDevice(adapter_.Get(), D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&device_)));
+}
---
+++ DirectX12Project_Client/DirectX12Project_Client/Core/DeviceManager.h
@@ -0,0 +1,31 @@
+#pragma once
+
+class DeviceManager
+{
+public:
+    static DeviceManager& GetInstance();
+
+    DeviceManager(const DeviceManager&) = delete;
+    DeviceManager& operator=(const DeviceManager&) = delete;
+    DeviceManager(DeviceManager&&) = delete;
+    DeviceManager& operator=(DeviceManager&&) = delete;
+
+private:
+    DeviceManager() = default;
+    ~DeviceManager() = default;
+
+public:
+    void Initialize();
+    ID3D12Device* GetDevice();
+
+private:
+    void CreateFactory();
+    void CreateAdapter();
+    void CreateDevice();
+
+private:
+    Microsoft::WRL::ComPtr<ID3D12Device> device_;
+    Microsoft::WRL::ComPtr<IDXGIFactory6> factory_;
+    Microsoft::WRL::ComPtr<IDXGIAdapter4> adapter_;
+};
+
